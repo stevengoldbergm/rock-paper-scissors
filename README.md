75e4f05c0cb46b4e05f33d9561bc4e5d3d02165d
SG_Demi <stevengoldbergm@gmail.com>
2022-08-17 22:49:52 -0500

    Created a README and uploaded a drawing to spice things up.

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # rock-paper-scissors
-Player vs Computer: Rock, Paper, Scissors!
+![Drawing of hands playing Rock, Paper, Scissors](./assets/images/rock-paper-scissors.jpg)
+
+## Description
+This in-browser app allows a single user to play a game of Rock, Paper, Scissors with a computer oponent. 
+It has the following features:
+- Pop-ups with reactive messages based on user-input
+- Looping gameplay. Test your luck as many times as you want!
+- A scorecard to keep track of who's champ and who's chump.
+
+
+## Installation
+N/A
+
+## Usage
+
+See the published site: [here](https://stevengoldbergm.github.io/rock-paper-scissors/)
+- To use the webpage, simply follow the prompts on the screen
+- If you decide to stop playing, or you don't follow the on-screen instructions, the game will stop 
+- To restart the game, simply refresh your browser window
+
+## License
+N/A
+
+
--- assets/js/complete.js
+++ assets/js/complete.js
@@ -2,23 +2,28 @@
 // // // Write the pop-up window code
 // // // Do you want to play?
 // // // User Function, enter R, P, or S
-function letsPlay() {
-    if (window.confirm("Do you want to play a game?")) {
-        console.log("They decided to play!");
-        RPS();
-    } else {
-        window.alert("Maybe next time. . .");
-        console.log("Fun over. . .");
-        return;
-    };
-}
-
+var playAgain = true
+// while (playAgain) {
+//     letsPlay();
+// }
 // Keep global variables out of the functions!
 var tie = 0
 var win = 0
 var lose = 0
 
 letsPlay();
+
+function letsPlay() {
+    if (window.confirm("Are you ready for some Rock, Paper, Scissors?!")) {
+        console.log("They decided to play!");
+        RPS();
+    } else {
+        window.alert("Maybe next time. . .");
+        console.log("Fun over. . .");
+        playAgain = false;
+        return;
+    };
+}
 
 function RPS() {
         var userOptions = ["ROCK", "PAPER", "SCISSORS"]
@@ -27,23 +32,26 @@ function RPS() {
         var userChoice = prompt("Type Rock, Paper, or Scissors!");
         console.log(userChoice)
         if (userChoice === null) {
+            playAgain = false;
             return;
         }
 
-        console.log(userChoice.toUpperCase())
+        userChoice = userChoice.toLocaleUpperCase()
+        console.log(userChoice)
 
-        if (userChoice.toUpperCase() == userOptions[0] || userChoice.toUpperCase() == userOptions[1] || userChoice.toUpperCase() == userOptions[2]) {
-                window.alert("You chose " + userChoice.toUpperCase() + "!");
-                console.log(userChoice.toUpperCase());
+        if (userChoice == userOptions[0] || userChoice == userOptions[1] || userChoice == userOptions[2]) {
+                window.alert("You chose " + userChoice + "!");
+                console.log(userChoice);
         } 
         else {
 
             window.alert("Was that a typo, or were you just being funny?");
-            // return;
+            playAgain = false;
+            return;
         };
 
         console.log(userChoice)
-        userChoice = userChoice.toLocaleUpperCase()
+
     // Computer function randomizes R, P, or S
     // The computer makes a choice!
         var compChoice = compOptions[Math.floor(Math.random() * compOptions.length)];
@@ -66,12 +74,25 @@ function RPS() {
             console.log(lose)
         }
 
-    // // Current score
-    // // Do you want to play again?
-        if (window.confirm("You have won "+ win + " rounds, lost " + lose + " rounds, and tied " + tie + " rounds. Would you like to play again?")) {
-            RPS();
-        } else {
-            window.alert("Some other time, then. . .");
-            leave = true;
-        }
+    // // // Current score
+    // // // Do you want to play again?
+    //     if (window.confirm("You have won "+ win + " rounds, lost " + lose + " rounds, and tied " + tie + " rounds. Would you like to play again?")) {
+    //         RPS();
+    //     } else {
+    //         window.alert("Some other time, then. . .");
+    //         leave = true;
+    //     }
 }
+
+// Current score
+// Do you want to play again?
+
+    while (playAgain) {
+        if (window.confirm("You have won "+ win + " rounds, lost " + lose + " rounds, and tied " + tie + " rounds. Would you like to play again?")) {
+                RPS();
+            } else {
+                window.alert("Some other time, then. . ."); 
+                playAgain = false
+            }
+    }
+    
